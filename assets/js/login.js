305442f7ab55d3de4668233b000816d627c4b5eb
kabuda <943019067@qq.com>
2020-06-17 12:06:24 +0800

    上午完成案例

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,87 +1,93 @@
 $(function () {
-    // ----------------  切换登录和注册的盒子 -----------------
-    $('.goto-register a').click(function () {
-        $('#register').show().prev().hide();
-    });
-    $('.goto-login a').click(function () {
-        $('#register').hide().prev().show();
-    });
-
-    // ----------------  注册功能  --------------------------
-    // 监听注册表单的提交事件
-    $('#register form').on('submit', function (e) {
-        
-        // 阻止默认行为（阻止表单提交的行为）
-        e.preventDefault();
-        // 使用JS收集表单中的数据(获取输入框中的账号和密码)
-        /**
-         * serialize 是根据表单项的name属性值获取值的，所以这里一定要检查表单项的name属性
-         */
-        var data = $(this).serialize();
-        // console.log(data); // username=admin&password=123
-        // 把账号和密码提交给接口，从而完成注册
-        $.ajax({
-            type: 'POST',
-            url: 'http://btapi.ehomespace.com/api/reguser',
-            data: data,
-            success: function (res) {
-                // 无论成功还是失败，都要提示
-                // alert(res.message);
-                layer.msg(res.message);
-                // 并且隐藏注册的盒子，显示登录的盒子
-                if (res.status === 0) {
-                    $('#register').hide().prev().show();
-                }
-            }
-        });
-        
-    });
-
-    // ----------------  完成表单验证 ------------------------
-    var form = layui.form;
-    // console.log(form); // 得到一个对象
-    // 调用form.verify() 方法，自定义验证规则
+    // layui是全局对象，通过它可以得到form对象
+    var form = layui.form
+    // 基于LayUI自定义表单验证规则
     form.verify({
-        // 键（验证规则名称）: 值（验证方法，可以使用数组，也可以使用函数）,
-        // len: [正则表达式, 错误提示],
-        len: [/^\w{6,12}$/, '密码长度必须是6到12位'], //{6,12}不要写成{6, 12}
-
+        // 必须是6-8位字符,不包括空格
+        uname: [/^[\S]{6,8}$/, '用户名必须是6-8位字符'],
+        // 密码必须是6位数字
+        pwd: function (value, item) {
+            // 形参value标书对应输入域的值
+            // item表示DOM元素
+            // 验证6位数字
+            var reg = /^\d{6}$/
+            // 如果规则不匹配就返回提示
+            if (!reg.test(value)) {
+                return '密码必须是6位数字'
+            }
+        },
+        // 验证确认密码必须和原有密码一致
         same: function (value) {
-            // 形参 value 表示使用验证规则的输入的值
-            // return '错误提示';
-            // 获取密码框的值
-            var password = $('#reg-password').val();
-            // 比较 password（密码）和value（确认密码）
-            if (password !== value) {
-                return '两次密码不一致-laotang';
+            // 获取原始密码
+            var pwd = $('#registerForm input[name=password]').val()
+            if (pwd !== value) {
+                return '两次输入的密码必须一致'
             }
         }
-    });
+    })
 
+    // 控制登录表单的提交
+    $('#loginForm').submit(function (e) {
+        // 阻止表单默认提交行为
+        e.preventDefault()
+        // 获取表单输入域的用户名和密码
+        // username=asdf&password=asdffffff
+        var formData = $(this).serialize()
+        // 提交表单之前需要做表单验证，如果自己实现有点繁琐，所以可以借助LayUI实现
 
-    // ------------------ 完成登录功能代码 -------------------
-    // 监听登录表单提交事件
-    $('#login form').on('submit', function (e) {
-
-        // 阻止默认行为
-        e.preventDefault();
-        // 获取账号和密码
-        // ajax提交给接口
+        // 调用后台接口验证是否正确
         $.ajax({
-            type: 'POST',
-            url: 'http://btapi.ehomespace.com/api/login',
-            data: $(this).serialize(),
+            type: 'post',
+            url: 'http://ajax.frontend.itheima.net/api/login',
+            data: formData,
             success: function (res) {
-                // 无论成功，还是失败，都给出提示
-                // alert(res.message);
-                layer.msg(res.message);
-                // 成功了，自己保存token，跳转到index.html
+                // 登录成功后，跳转到主页面
                 if (res.status === 0) {
-                    // 保存到本地存储中
-                    localStorage.setItem('token', res.token);
-                    location.href = './index.html';
+                    // 把登录成功的标志位存储在客户端
+                    localStorage.setItem('mytoken', res.token)
+                    // 跳转到主页面
+                    location.href = './index.html'
                 }
             }
-        });
-    });
-});
+        })
+    })
+
+    // 控制注册表单的提交
+    $('#registerForm').submit(function (e) {
+        e.preventDefault()
+        // 获取表单数据(表单输入域必须提供name属性，name的值必须和接口文档要求一致)
+        var formData = $(this).serialize()
+        // 调用接口进行注册
+        $.ajax({
+            type: 'post',
+            url: 'http://ajax.frontend.itheima.net/api/reguser',
+            data: formData,
+            success: function (res) {
+                if (res.status === 0) {
+                    // 注册成功，显示登陆框
+                    $('#registerForm a').click()
+                    // 成功后进行提示
+                    layer.msg(res.message)
+                } else {
+                    // 注册失败
+                    // layer是一个独立的模块，默认可以直接使用
+                    layer.msg(res.message)
+                }
+            }
+        })
+    })
+
+    // 登录表单的底部链接
+    $('#loginForm a').click(function () {
+        $('#loginForm').hide()
+        $('#registerForm').show()
+    })
+
+    // 注册表单的地步链接
+    $('#registerForm a').click(function () {
+        // 点击后应该做什么？切换表单的显示和隐藏
+        $('#loginForm').show()
+        $('#registerForm').hide()
+    })
+
+})
